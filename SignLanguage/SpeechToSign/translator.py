114625bbad8cb942382ce1407ae1a61e04fe65c2
Harikeshav R <r.harikeshav@gmail.com>
2023-07-26 13:38:16 +0530

    Create ISLConverter class

--- SignLanguage/SpeechToSign/translator.py
+++ SignLanguage/SpeechToSign/translator.py
@@ -1,9 +1,11 @@
 import nltk
-from nltk.parse.stanford import StanfordParser
-from nltk.tag.stanford import StanfordPOSTagger, StanfordNERTagger
-from nltk import word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
 from nltk.stem import PorterStemmer
+from nltk.tree import ParentedTree
 
-input_string = input("> ")
+
+class ISLConverter:
+    def __init__(self):
+        self.parser = nltk.parse.corenlp.CoreNLPParser(url='http://localhost:9000')
+        self.stemmer = PorterStemmer()
+        self.stop_words = set(stopwords.words("english"))
