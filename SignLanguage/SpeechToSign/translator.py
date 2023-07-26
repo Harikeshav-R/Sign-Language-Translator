eea07269996584e244323044d1386e9cf7958a73
Harikeshav R <r.harikeshav@gmail.com>
2023-07-26 14:19:40 +0530

    Add tense recognition to translator

--- SignLanguage/SpeechToSign/translator.py
+++ SignLanguage/SpeechToSign/translator.py
@@ -1,6 +1,8 @@
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
 from nltk.tree import ParentedTree
 
 
@@ -77,6 +79,38 @@ class ISLConverter:
 
         return parsed_tree
 
+    def get_tense(self, input_string: str) -> str:
+        """
+        Get the tense of the input English sentence.
+
+        Args:
+            input_string (str): The input English sentence.
+
+        Returns:
+            str: The tense of the sentence (e.g., "past", "present", "future").
+        """
+        # Tokenize and part-of-speech tag the input sentence
+        tokens = word_tokenize(input_string)
+        pos_tags = pos_tag(tokens)
+
+        # Map POS tags to simplified tense categories
+        tense_map = {
+            "VBD": "past",  # Past tense verb
+            "VB": "present",  # Base form (present tense) verb
+            "VBZ": "present",  # Present tense verb with 3rd person singular
+            "MD": "future",  # Modal verb indicating future tense
+            "VBP": "future"  # Non-3rd person singular present tense verb (will)
+        }
+
+        # Check for verb POS tags and determine the tense
+        verb_tenses = [tense_map[tag] for _, tag in pos_tags if tag in tense_map]
+
+        # Return the most common tense or "unknown" if no verbs found
+        if verb_tenses:
+            return max(set(verb_tenses), key=verb_tenses.count)
+        else:
+            return "unknown"
+
     def convert_to_isl(self, input_string: str) -> str:
         """
         Convert an input English sentence to Indian Sign Language (ISL) sentence.
@@ -87,6 +121,10 @@ class ISLConverter:
         Returns:
             str: The converted ISL sentence.
         """
+
+        # Get the tense
+        tense = self.get_tense(input_string)
+
         # Parse the English text
         english_tree = [tree for tree in self.parser.parse(input_string.split())]
         parse_tree = english_tree[0]
@@ -116,4 +154,17 @@ class ISLConverter:
         if question_word:
             isl_sentence.append(question_word)
 
+        # Example representation of tense in ISL sentence
+        if tense == "past":
+            isl_sentence.append("before")
+        elif tense == "future":
+            isl_sentence.append("after")
+
         return " ".join(isl_sentence)
+
+
+if __name__ == "__main__":
+    converter = ISLConverter()
+    input_string = input("> ")
+    isl_sentence = converter.convert_to_isl(input_string)
+    print("Indian Sign Language Text:", isl_sentence)
