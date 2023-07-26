8d7b8ee6fb02177516ef2962903b7876ab7e61bc
Harikeshav R <r.harikeshav@gmail.com>
2023-07-26 13:39:37 +0530

    Reorder the parse tree to follow Indian Sign Language grammar needs

--- SignLanguage/SpeechToSign/translator.py
+++ SignLanguage/SpeechToSign/translator.py
@@ -10,7 +10,7 @@ class ISLConverter:
         self.stemmer = PorterStemmer()
         self.stop_words = set(stopwords.words("english"))
 
-    def find_leaf_position(self, parsed_tree: ParentedTree, subtree: ParentedTree) -> tuple:
+    def find_leaf_position(self, parsed_tree: ParentedTree, subtree: ParentedTree) -> tuple | None:
         """
         Find the leaf position of the given subtree in the parse tree.
 
@@ -27,3 +27,30 @@ class ISLConverter:
             return parsed_tree.leaf_treeposition(subtree_index)[:-1]
         except ValueError:
             return None
+
+    def reorder_for_isl(self, parsed_tree: ParentedTree) -> ParentedTree:
+        """
+        Reorder the parse tree for Indian Sign Language (ISL) grammar needs.
+
+        Args:
+            parsed_tree (ParentedTree): The parsed tree of the sentence.
+
+        Returns:
+            ParentedTree: The reordered parse tree.
+        """
+        # Find NP and VP subtrees in the parse tree
+        noun_phrases = [subtree for subtree in parsed_tree.subtrees(lambda t: t.label() == "NP")]
+        verb_phrases = [subtree for subtree in parsed_tree.subtrees(lambda t: t.label() == "VP")]
+
+        # Rearrange VP subtrees to the right of the corresponding NP subtrees
+        for np_subtree, vp_subtree in zip(noun_phrases, verb_phrases):
+            # Get the positions of NP and VP subtrees
+            np_position = self.find_leaf_position(parsed_tree, np_subtree)
+            vp_position = self.find_leaf_position(parsed_tree, vp_subtree)
+
+            # Rearrange VP subtree to the right of the NP subtree
+            if np_position and vp_position and vp_position[-1] < np_position[-1]:
+                del parsed_tree[vp_position]
+                parsed_tree[np_position[-1] + 1:np_position[-1] + 1] = [vp_subtree]
+
+        return parsed_tree
