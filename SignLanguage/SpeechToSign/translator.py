97c0a9bc57d87f2b811abdcabbe325d183d39b7e
Harikeshav R <r.harikeshav@gmail.com>
2023-07-26 13:39:00 +0530

    Create function that finds the leaf position of the given subtree in the parse tree

--- SignLanguage/SpeechToSign/translator.py
+++ SignLanguage/SpeechToSign/translator.py
@@ -9,3 +9,21 @@ class ISLConverter:
         self.parser = nltk.parse.corenlp.CoreNLPParser(url='http://localhost:9000')
         self.stemmer = PorterStemmer()
         self.stop_words = set(stopwords.words("english"))
+
+    def find_leaf_position(self, parsed_tree: ParentedTree, subtree: ParentedTree) -> tuple:
+        """
+        Find the leaf position of the given subtree in the parse tree.
+
+        Args:
+            parsed_tree (ParentedTree): The parsed tree of the sentence.
+            subtree (ParentedTree): The subtree whose position needs to be found.
+
+        Returns:
+            tuple: The leaf position of the subtree in the parse tree, or None if not found.
+        """
+        leaves = parsed_tree.leaves()
+        try:
+            subtree_index = leaves.index(subtree.leaves()[0])
+            return parsed_tree.leaf_treeposition(subtree_index)[:-1]
+        except ValueError:
+            return None
