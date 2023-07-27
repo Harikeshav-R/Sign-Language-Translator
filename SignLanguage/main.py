07fa73633c48d6cd1c458646b6c3b8049905eb37
Harikeshav R <r.harikeshav@gmail.com>
2023-07-27 10:55:46 +0530

    Create final GUI converter

--- SignLanguage/main.py
+++ SignLanguage/main.py
@@ -3,12 +3,33 @@
 
 from SpeechToSign.compiler import VideoDisplayer
 from SpeechToSign.translator import ISLConverter
+from ui_main import *
 
-sentence = input("> ")
-converter = ISLConverter()
-video_displayer = VideoDisplayer()
 
-parsed = converter.convert_to_isl(sentence).split()
-print(parsed)
-video_displayer.show(parsed)
-video_displayer.destroy_delayed()
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        # Setup the UI from the UI file.
+        self.setupUi(self)
+
+        self.convert_button.clicked.connect(self.translate)
+
+    def translate(self):
+        converter = ISLConverter()
+        video_displayer = VideoDisplayer()
+
+        sentence = self.input_field.text()
+
+        parsed = converter.convert_to_isl(sentence).split()
+        video_displayer.show(parsed)
+        video_displayer.destroy_delayed()
+
+
+app = QApplication([])
+app.setStyle("Fusion")
+
+window = MainWindow()
+window.show()
+
+app.exec()
